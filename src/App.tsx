a7156de69617cc2ae423f78c73dd9896bb2ec172
tkjelds <tkjelds385@gmail.com>
2026-02-13 18:10:57 +0100

    added placeholder files for content pages and refactored menu to be more extensible

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,39 @@
 import "./index.css";
 
-import { useState } from "react";
+import { useState, type JSX } from "react";
+
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+
+interface IMenuItem {
+  label : string;
+  component : JSX.Element;
+}
+
 
 export function App() {
-  enum MenuKey {
-    ABOUT,
-    CONTACT,
-    PROJECTS,
-  }
-
-  const [active, setActive] = useState(MenuKey.ABOUT);
-
-  const renderComponent = () => {
-    switch (active) {
-      case MenuKey.ABOUT :
-        return <h1>About</h1>;
-      case MenuKey.CONTACT :
-        return <h1>Contact</h1>;
-      case MenuKey.PROJECTS :
-        return <h1>Projects</h1>;
-      default:
-        return <h1>About</h1>;
-    }
-  };
-
-
+  
+  const MenuItems : IMenuItem[] = [
+    { label: "About", component: <About /> },
+    { label: "Projects", component: <Projects /> },
+    { label: "Contact", component: <Contact /> },
+  ];
+  const [active, setActive] = useState(MenuItems[0]);
 
   return (
-<div className="app">
-  <div className="content"> 
-      <div className="left">
-        <h1 className="menu_item" onClick={() => setActive(MenuKey.ABOUT)}>About</h1>
-        <h1 className="menu_item" onClick={() => setActive(MenuKey.PROJECTS)}>Projects</h1>
-        <h1 className="menu_item" onClick={() => setActive(MenuKey.CONTACT)}>Contact</h1>
+  <div className="app">
+    <div className="content"> 
+        <div className="left">
+          {MenuItems.map((item) => (
+            <h1 className="menu_item" onClick={() => setActive(item)}> {item.label} </h1>
+          ))}
+        </div>
+        <div className="right">
+          {active?.component}
+        </div>
       </div>
-      <div className="right">
-        {renderComponent()}
-      </div>
-    </div>
-</div>
+  </div>
   );
 }
 
---
+++ src/pages/About.tsx
@@ -0,0 +1,7 @@
+export default function About() {
+  return (
+    <div>
+      <h1>Abou djsfkljdsalk jf</h1>
+    </div>
+  );
+}
