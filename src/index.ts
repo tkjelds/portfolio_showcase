df4552859fb1bf7aaf0fe3615cdbcd84efb9e7b5
tkjelds <tkjelds385@gmail.com>
2026-02-16 13:09:16 +0100

    removed boilerplate routes

--- src/index.ts
+++ src/index.ts
@@ -5,28 +5,6 @@ const server = serve({
   routes: {
     // Serve index.html for all unmatched routes.
     "/*": index,
-
-    "/api/hello": {
-      async GET() {
-        return Response.json({
-          message: "Hello, world!",
-          method: "GET",
-        });
-      },
-      async PUT() {
-        return Response.json({
-          message: "Hello, world!",
-          method: "PUT",
-        });
-      },
-    },
-
-    "/api/hello/:name": async (req) => {
-      const name = req.params.name;
-      return Response.json({
-        message: `Hello, ${name}!`,
-      });
-    },
   },
 
   development: process.env.NODE_ENV !== "production" && {
