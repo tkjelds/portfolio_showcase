816d59627ca463beef2b1bd17ab9f9ee25814be5
tkjelds <tkjelds385@gmail.com>
2026-02-16 14:46:19 +0100

    added actual structure for project card, just missing content and styling for text

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -3,7 +3,7 @@ import { Splide, SplideSlide } from "@splidejs/react-splide";
 import i18n from "../i18n";
 // Default theme
 import "@splidejs/react-splide/css";
-
+import repos from "../data/projects.json";
 // or only core styles
 import "@splidejs/react-splide/css/core";
 
@@ -19,14 +19,26 @@ export default function Projects() {
     {
       title: i18n.t("section.projects.minitwit.title"),
       description: i18n.t("section.projects.minitwit.description"),
+      repositoryUrl: repos.minitwit.link,
+      imageUrls: repos.minitwit.imagesURL,
     },
     {
       title: i18n.t("section.projects.scooterSharing.title"),
       description: i18n.t("section.projects.scooterSharing.description"),
+      repositoryUrl: repos.scooterSharing.link,
+      imageUrls: repos.scooterSharing.imagesURL,
     },
     {
       title: i18n.t("section.projects.personalPortfolio.title"),
       description: i18n.t("section.projects.personalPortfolio.description"),
+      repositoryUrl: repos.personalPortfolio.link,
+      imageUrls: repos.personalPortfolio.imagesURL,
+    },
+    {
+      title: i18n.t("section.projects.mctssimulation.title"),
+      description: i18n.t("section.projects.mctssimulation.description"),
+      repositoryUrl: repos.mctssimulation.link,
+      imageUrls: repos.mctssimulation.imagesURL,
     },
   ];
   return (
@@ -34,7 +46,7 @@ export default function Projects() {
       <Splide
         options={{
           perPage: 1,
-          pagination: true,
+          pagination: false,
           arrows: true,
           type: "fade",
           rewind: true,
@@ -47,7 +59,32 @@ export default function Projects() {
           <SplideSlide key={index}>
             <div className="project_card">
               <h2>{project.title}</h2>
+              <Splide
+                options={{
+                  perPage: 1,
+                  pagination: false,
+                  type: "loop",
+                  rewind: true,
+                  rewindbydrag: true,
+                  click: false,
+                  arrows: false,
+                  autoplay: true,
+                }}
+              >
+                {project.imageUrls?.map((url, imageIndex) => (
+                  <SplideSlide key={imageIndex}>
+                    <img
+                      className="project_image"
+                      src={url}
+                      alt={`${project.title} image ${imageIndex + 1}`}
+                    />
+                  </SplideSlide>
+                ))}
+              </Splide>
               <p>{project.description}</p>
+              <button className="repository_button">
+                {i18n.t("Repository")}
+              </button>
             </div>
           </SplideSlide>
         ))}
